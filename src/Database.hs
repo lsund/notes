9129dcc7f95ea33e88f8244acfe71a5985a10808
Ludvig Sundstrom <ludvig.sundstroem@innoq.com>
2020-12-20 11:26:38 +0100

    Remove qualified imports in database, event

--- src/Database.hs
+++ src/Database.hs
@@ -4,43 +4,45 @@
 module Database where
 
 import Prelude hiding (writeFile)
-import qualified Note as N
-import Field
-import Data.String (fromString)
-import Data.Text.Encoding (decodeUtf8)
-import qualified Brick.Widgets.Edit as E
-import Data.ByteString.Lazy (toStrict)
-import qualified Brick.Focus as F
-import Data.Aeson (FromJSON, ToJSON, decode, encode)
 import GHC.Generics
 import Data.Text (Text)
 import Data.Text.IO (writeFile)
+import Data.String (fromString)
+import Data.Text.Encoding (decodeUtf8)
+import Data.ByteString.Lazy (toStrict)
+import Data.Aeson (FromJSON, ToJSON, decode, encode)
 
-data Note = Note { _id :: Integer, _active :: Bool, _locked :: Bool,  _title :: Text, _content :: Text }
+import qualified Brick.Widgets.Edit as Edit
+import qualified Brick.Focus as Focus
+
+import Field
+import Note
+
+data SerializedNote = SerializedNote { _id :: Integer, _active :: Bool, _locked :: Bool,  _title :: Text, _content :: Text }
     deriving (Generic, Show)
 
-instance FromJSON Note
+instance FromJSON SerializedNote
 
-instance ToJSON Note
+instance ToJSON SerializedNote
 
-internalize :: N.Note -> Note
-internalize (N.Note id active locked (Field title _) (Field content _) _) = Note id active locked title content
+internalize :: Note -> SerializedNote
+internalize (Note id active locked (Field title _) (Field content _) _) = SerializedNote id active locked title content
 
-notesToString :: [N.Note] -> Text
+notesToString :: [Note] -> Text
 notesToString = decodeUtf8 . toStrict  . encode . map internalize
 
-serialize :: FilePath -> [N.Note] -> IO ()
+serialize :: FilePath -> [Note] -> IO ()
 serialize file xs = writeFile file (notesToString xs)
 
 extractList :: Maybe [a] -> [a]
 extractList Nothing = []
 extractList (Just xs) = xs
 
-externalize :: Note -> N.Note
-externalize (Note id active locked title content) =
-    N.Note id active locked (Field title (E.editor id Nothing title)) (Field content (E.editor id Nothing content)) (F.focusRing [N.Title, N.Content])
+externalize :: SerializedNote -> Note
+externalize (SerializedNote id active locked title content) =
+    Note id active locked (Field title (Edit.editor id Nothing title)) (Field content (Edit.editor id Nothing content)) (Focus.focusRing [Title, Content])
 
-deserialize :: FilePath -> IO (Maybe [N.Note])
+deserialize :: FilePath -> IO (Maybe [Note])
 deserialize file = do
     content <- readFile file
     return $ map externalize <$> decode (fromString content)
